4579ec5b703c678ccede651f1c4d83dae5bc24ab
Patrick Conwell <patrick.conwell@gmail.com>
2017-12-06 00:19:37 -0600

    Update github-backup.sh

--- github-backup.sh
+++ github-backup.sh
@@ -1,4 +1,9 @@
+## Old
 hash='"'`curl  https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp | grep sha | cut -d '"' -f4`'"'
 temp64='"'`/bin/cli-shell-api showCfg --show-hide-secrets | base64 | tr -d '\n'`'"'
 curl -i -X PUT -H 'Authorization: token '$token'' -d '{"path": "config.bak", "message": "automated backup", "content": '"$temp64"', "sha": '"$hash"', "branch": "master"}' https://api.git
 
+## New (?)
+hash='"'`curl  https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp | grep sha | cut -d '"' -f4`'"'
+temp64='"'`/bin/cli-shell-api showCfg --show-hide-secrets | base64 | tr -d '\n'`'"'
+curl --request PUT --user "pconwell:TOKEN" --data '{"message": "automated backup", "content": '"$temp64"', "sha": '"$hash"'}' https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp
