48e2a0e25224392e775f0b93f06a4d7621ffd8f4
Patrick Conwell <patrick.conwell@gmail.com>
2018-02-28 02:52:30 -0600

    added check to see if a change exists

--- github-backup.sh
+++ github-backup.sh
@@ -1,3 +1,17 @@
+temp1='"'`curl --request GET --user "USER:TOKEN" https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp | jq -r '.content' | tr -d '\n'`'"'
+
 hash='"'`curl  https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp | grep sha | cut -d '"' -f4`'"'
 temp64='"'`/bin/cli-shell-api showCfg --show-hide-secrets | base64 | tr -d '\n'`'"'
-curl --request PUT --user "pconwell:TOKEN" --data '{"message": "automated backup", "content": '"$temp64"', "sha": '"$hash"'}' https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp
+
+if [ "$temp1" == "$temp64" ];
+
+then
+
+	echo true;
+	
+else
+
+curl --request PUT --user "USER:TOKEN" --data '{"message": "automated backup", "content": '"$temp64"', "sha": '"$hash"'}' https://api.github.com/repos/pconwell/edgerouter/contents/config.boot.erxsfp
+
+
+fi
